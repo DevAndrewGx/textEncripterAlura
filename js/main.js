819a2b9a541939973248d2202538eafeae1e3f7b
AndrewGx <128672267+DevAndrewGx@users.noreply.github.com>
2023-08-16 12:01:31 -0500

    Adding new functions and correct mistakes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 // We need to create our variables to encript text
-const text = document.getElementById("texto");
+let text = document.getElementById("texto");
+const imgToy = document.getElementById("img-toy");
+const mssNotFound = document.getElementById("message-nfound");
 
 // We're going to validate if the data have accent and if the data is UpperCase
 
-function isUpperCaseWithAccent(text) {
+function isUpperCase(text) {
     // We have to create regex to evaluate the string
     const regex = /[A-ZáéíóúÁÉÍÓÚ]/;
     const tieneMayusculasYAcentos = regex.test(text);
 
     return tieneMayusculasYAcentos;
 }
-
 
 function withAccent(text) {
     // We need to validate that the string has no weird characteres
@@ -20,12 +21,17 @@ function withAccent(text) {
     return tieneCaracteresRaros;
 }
 
-
-
+//Here we're going to use this function to encript the text.
 function encript() {
-    if (isUpperCaseWithAccent(text.value) || withAccent(text.value)) {
-        alert("El texto esta mayuscula o contiene caracteres que no son alfabeticos");
-    }else {
+    if (isUpperCase(text.value) || withAccent(text.value)) {
+        alert(
+            "El texto esta mayuscula o contiene caracteres que no son alfabeticos"
+        );
+        text.value = "";
+    } else {
+        imgToy.style.display = "none";
         console.log("No tiene caracteres raros");
     }
 }
+
+function decript() {}
