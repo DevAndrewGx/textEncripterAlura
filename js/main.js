f91bdee7bb8ac2c34887a6a9fa4c05114927fd9a
AndrewGx <128672267+DevAndrewGx@users.noreply.github.com>
2023-08-19 19:05:04 -0500

    fixing erros and add new functions

--- js/main.js
+++ js/main.js
@@ -1,37 +1,48 @@
 // We need to create our variables to encript text
-let text = document.getElementById("texto");
+let text = document.getElementById("textoArea");
 const imgToy = document.getElementById("img-toy");
-const mssNotFound = document.getElementById("message-nfound");
+const mssNotFound = document.getElementById("message-not-found");
+const spanMessage = document.getElementById("change-text");
+const encryptedContainer = document.querySelector(".encrypted-container");
+
+encryptedContainer.style.display = 'none';
 
 // We're going to validate if the data have accent and if the data is UpperCase
 
-function isUpperCase(text) {
-    // We have to create regex to evaluate the string
-    const regex = /[A-ZáéíóúÁÉÍÓÚ]/;
-    const tieneMayusculasYAcentos = regex.test(text);
 
-    return tieneMayusculasYAcentos;
-}
-
-function withAccent(text) {
+function validateText(text) {
     // We need to validate that the string has no weird characteres
-    const regex = /[^A-Za-z]/;
-    const tieneCaracteresRaros = regex.test(text);
-
-    return tieneCaracteresRaros;
+    const regex = /^[a-z ]+$/;
+    return regex.test(text);
 }
 
 //Here we're going to use this function to encript the text.
 function encript() {
-    if (isUpperCase(text.value) || withAccent(text.value)) {
-        alert(
-            "El texto esta mayuscula o contiene caracteres que no son alfabeticos"
-        );
+    if (!validateText(text.value)) {
+        alert("El texto esta mayuscula o contiene caracteres que no son alfabeticos");
         text.value = "";
     } else {
+        let encryptedText = text.value.replace(/e/gi, "enter");
+        encryptedText = encryptedText.replace(/i/gi, "imes");
+        encryptedText = encryptedText.replace(/a/gi, "ai");
+        encryptedText = encryptedText.replace(/o/gi, "ober");
+        encryptedText = encryptedText.replace(/u/gi, "ufat");
+        console.log(encryptedText);
+
         imgToy.style.display = "none";
-        console.log("No tiene caracteres raros");
+        mssNotFound.style.display = "none";
+        spanMessage.innerHTML = encryptedText;
+        encryptedContainer.style.display = "block";
     }
 }
 
-function decript() {}
+function decript() {
+
+    let decript = text.value.replace(/enter/gi, "e");
+    decript = decript.replace(/imes/gi, "i");
+    decript = decript.replace(/ai/gi, "a");
+    decript = decript.replace(/ober/gi, "o");
+    decript = decript.replace(/ufat/gi, "u");
+
+    spanMessage.innerHTML = decript;
+}
